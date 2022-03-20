f0b62568b33bcf4787ddab646cf0054fc7b54fc4
Alexander Musienko <alexmvid0@gmail.com>
2022-03-20 12:39:11 +0300

    .

--- animation.js
+++ animation.js
@@ -2,6 +2,20 @@ const formAnim = document.getElementById("form");
 const submitOutput = document.getElementById("output");
 var submit = 1;
 
+const day = formAnim.querySelector('[name="day"]'),
+month = formAnim.querySelector('[name="month"]'),
+lenses = formAnim.querySelector('[name="lensesExpiration"]');
+
+var formValue = { 
+  day: day,
+  month: month,
+  lenses: lenses
+}
+
+function formBuffer() { 
+
+}
+
 function outputAnim() {
   if (submit % 2 == 0) {
     submitOutput.style = "animation-name: outputClose;";
@@ -16,10 +30,18 @@ function submitClick() {
   return submit;
 }
 
-function submitAnim() { 
-  document.getElementById("submitAnim").style = "animation-name: submit;"
+function submitAnim() {
+  document.getElementById("submitAnim").style = "animation-name: submit;";
 }
+
+/*function staticOutTransition() {
+  if (day == day && month == month && lenses == lenses) {
+    submitOutput.style = "animation-name: staticOut;"
+  }
+}
+*/
 
 formAnim.addEventListener("submit", outputAnim);
 formAnim.addEventListener("submit", submitClick);
-formAnim.addEventListener("submit", submitAnim);
+//formAnim.addEventListener("submit", submitAnim);
+formAnim.addEventListener("submit", formBuffer);
