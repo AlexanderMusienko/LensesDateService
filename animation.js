9d685a555065be3beab342d2ed75a587f7d5fa6a
Alexander Musienko <alexmvid0@gmail.com>
2022-03-20 18:10:06 +0300

    swipe output

--- animation.js
+++ animation.js
@@ -1,32 +1,40 @@
 const formAnim = document.getElementById("form");
 const submitOutput = document.getElementById("output");
 var submit = 1;
+var day, month, lenses;
 
-const day = formAnim.querySelector('[name="day"]'),
-month = formAnim.querySelector('[name="month"]'),
-lenses = formAnim.querySelector('[name="lensesExpiration"]');
-
-var formValue = { 
-  day: day,
-  month: month,
-  lenses: lenses
+function retrieveValue() {
+day = formAnim.querySelector('[name="day"]').value,
+month = formAnim.querySelector('[name="month"]').value,
+lenses = formAnim.querySelector('[name="lensesExpiration"]').value;
 }
 
-function formBuffer() { 
+var formValue = [day, month, lenses];
 
-}
+function formMemory() {
+  console.log(month);
+  formValue.push(day, month, lenses);
+  console.log(`Длина массива: ${formValue.length}`);
+  if (formValue.length > 6) {
+    formValue.splice(0, 3);
+    console.log(`Длина массива после удаления: ${formValue.length}`);
+    console.log(`Массив старый: ${formValue[0]};${formValue[1]};${formValue[2]}`);
+    console.log(`Массив новый: ${formValue[3]};${formValue[4]};${formValue[5]}`);
+  }
+};
+
 
 function outputAnim() {
   if (submit % 2 == 0) {
-    submitOutput.style = "animation-name: outputClose;";
-  } else {
-    submitOutput.style = "animation-name: output;";
+    (formValue[0] != formValue[3]) ? submitOutput.style = "animation-name: outputSwipe;" : submitOutput.style = "animation-name: outputClose;";  
+  } else {  
+    submitOutput.style = "animation-name: output;"; 
   }
-}
+  }
 
 function submitClick() {
   submit++;
-  console.log(submit);
+  console.log(`Нажатие: ${submit}`);
   return submit;
 }
 
@@ -43,5 +51,5 @@ function submitAnim() {
 
 formAnim.addEventListener("submit", outputAnim);
 formAnim.addEventListener("submit", submitClick);
-//formAnim.addEventListener("submit", submitAnim);
-formAnim.addEventListener("submit", formBuffer);
+formAnim.addEventListener("submit", retrieveValue);
+formAnim.addEventListener("submit", formMemory);
