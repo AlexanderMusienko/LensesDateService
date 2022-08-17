65d39b630c834e5ff418aa9b969a20f3b1451f63
Alexander Musienko <alexmvid0@gmail.com>
2022-08-18 01:16:16 +0300

    skeleton with some style solutions

--- date_calc.js
+++ date_calc.js
@@ -1,46 +1,20 @@
-const form = document.getElementById("form");
-var today = new Date();
-var formValues;
-function retrieveFormValue(event) {
+var form = document.getElementById("main-form");
+
+var inputInfo = {};
+
+function getInputInfo(event) {
   event.preventDefault();
 
-  const day = form.querySelector('[name="day"]'),
-    month = form.querySelector('[name="month"]'),
-    lenses = form.querySelector('[name="lensesExpiration"]');
-
-  formValues = {
-    day: day.value,
-    month: month.value,
-    lenses: lenses.value,
+  inputInfo = {
+    day: document.getElementById("day-input").value,
+    month: document.getElementById("month-input").value,
+    expiration: form.querySelector(".option-container").value,
   };
+  console.log(inputInfo);
 }
 
-function retrieveDesiredBuyDate() {
-  var buyDate = new Date(
-    `${today.getFullYear()}.${formValues.month}.${formValues.day}`
-  );
-  var desiredBuyDate = new Date(
-    `${today.getFullYear()}.${formValues.month}.${formValues.day}`
-  );
-  desiredBuyDate.setDate(desiredBuyDate.getDate() + +formValues.lenses);
-  var timeLeft =
-    (Date.parse(desiredBuyDate) - Date.parse(today)) / (60 * 60 * 24 * 1000);
-  timeLeft = timeLeft.toFixed();
-  
-  var options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  var easyDesiredBuyDate = desiredBuyDate.toLocaleDateString('en-US', options);
-
-  console.log(
-    `Дата покупки: ${buyDate}, Планируемая дата новой покупки: ${desiredBuyDate}, Оставшееся число дней: ${timeLeft}`
-  );
-
-  document.querySelector('.landing-output-container').innerHTML = `Next purchase: <br>${easyDesiredBuyDate}, <br>Days left: ${timeLeft}`;
-
+const dateInfo = {
+  todayDate: new Date(),
 }
 
-form.addEventListener("submit", retrieveFormValue);
-form.addEventListener("submit", retrieveDesiredBuyDate);
+form.addEventListener("submit", getInputInfo);
