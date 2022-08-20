37420ea6839dc51de99c0c475288dbe5b0ca36a9
AlexanderMusienko <101179294+AlexanderMusienko@users.noreply.github.com>
2022-08-20 15:09:32 +0300

    base calc logic done

--- date_calc.js
+++ date_calc.js
@@ -1,37 +1,42 @@
-var form = document.getElementById("main-form");
+const form = document.getElementById("main-form");
 
-var inputInfo = {};
-var dateInfo = {};
-var outputResult = {};
+const inputInfo = {};
+const dateInfo = {};
+const outputResult = {};
+
+function transformDayToMs(day) {  
+  return (day * 24 * 60 * 60 * 1000)
+}
 
 function getInputInfo(event) {
   event.preventDefault();
 
-  inputInfo = {
-    day: document.getElementById("day-input").value,
-    month: document.getElementById("month-input").value,
-    expiration: form.querySelector(".option-container").value,
-  };
+  inputInfo.day = document.getElementById("day-input").value;
+  inputInfo.month = document.getElementById("month-input").value;
+  inputInfo.expiration = form.querySelector(".option-container").value;
 
   console.log(inputInfo);
 }
 
-function getOpenDate() {  
-   
-  dateInfo = {
-    todayDate: new Date(),
-    openDate: new Date(`${inputInfo.month}/${inputInfo.day}/${new Date().getFullYear()}`), // month | day | year
-  }
+function getOpenDate(event) {  
+  event.preventDefault();
 
-  console.log(`Текущая дата: ${dateInfo.todayDate}`)
-  console.log(`Дата открытия: ${dateInfo.openDate}`)
+  dateInfo.todayDate = new Date();
+  dateInfo.openDate = new Date(`${inputInfo.month}/${inputInfo.day}/${new Date().getFullYear()}`);
+
+  console.log(`Текущая дата: ${dateInfo.todayDate}`);
+  console.log(`Дата открытия: ${dateInfo.openDate}`);
 } 
 
 function calcDate() { 
-  outputResult = {  
-    resultDate: dateInfo.openDate.getTime() + 
-  }
-}
+
+  outputResult.nextDateMs = dateInfo.openDate.getTime() + (transformDayToMs(inputInfo.expiration));
+  outputResult.nextDate = new Date(outputResult.nextDateMs);
+
+  console.log(`Следующая дата в мс: ${outputResult.nextDateMs}`);
+  console.log(`Следующая дата: ${outputResult.nextDate}`);
+} 
 
 form.addEventListener("submit", getInputInfo);
 form.addEventListener("submit", getOpenDate);
+form.addEventListener("submit", calcDate);
